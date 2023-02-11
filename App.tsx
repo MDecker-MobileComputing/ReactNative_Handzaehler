c38066f8e5e938767f809ec024d29464e59717d5
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-02-11 20:11:17 +0100

    Konstanten isDarkMode und backgroundStyle entfernt

--- App.tsx
+++ App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
   
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <View style={styles.container}>
