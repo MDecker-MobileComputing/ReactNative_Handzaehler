df18755191c08617aa385230231d515bb436f8b7
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-02-11 19:51:15 +0100

    UI fuer Handzaehler eingebaut, hat aber noch keine Funktion

--- App.tsx
+++ App.tsx
@@ -1,59 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import { TouchableOpacity, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+  
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -63,56 +11,64 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+
+      <TouchableOpacity>
+        <Text style={styles.buttonText}>Plus</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.zaehlerText}>0</Text>
+
+      <TouchableOpacity>
+        <Text style={styles.buttonText}>Minus</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.buttonReset}>Reset</Text>
+      </TouchableOpacity>
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    width: "100%",
+    marginHorizontal: "auto",
+    marginTop: "20%"
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  zaehlerText: {
+    fontSize: 50,
+    paddingTop: 30,
+    paddingBottom: 25,
+    fontWeight: "bold",
+    marginHorizontal: "auto",
+    textAlign: "center"
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  buttonText: {
+    fontSize: 30,
+    backgroundColor: "cyan",
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingTop: 20,
+    paddingBottom: 10,
+    marginHorizontal: 10,
+    textAlign: "center"
   },
-  highlight: {
-    fontWeight: '700',
-  },
+  buttonReset: {
+    fontSize: 30,
+    backgroundColor: "orange",
+    marginTop: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    paddingTop: 20,
+    paddingBottom: 10,
+    marginHorizontal: 10,
+    textAlign: "center"
+  }
 });
 
 export default App;
